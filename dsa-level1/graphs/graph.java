f409eb90c413d3d5f6b6119272d6b839f38ff96d
Thammineni <mithun.ram.kumar.thammineni@sap.com>
2021-06-20 14:26:18 +0530

    updated file

--- dsa-level1/graphs/graph.java
+++ dsa-level1/graphs/graph.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.util.*;
 
+import org.graalvm.compiler.lir.gen.PhiResolver;
+
 public class graph {
     public static class Edge {
         int src;
@@ -295,20 +297,354 @@ public class graph {
 
     }
 
+    public static void hamiltonian(ArrayList<Edge>[] graph, int src, int osrc, HashSet<Integer> vis, String psf) {
+        if (vis.size() == graph.length - 1) {
+            System.out.print(psf + src);
+
+            boolean isCyclic = false;
+            for (Edge e : graph[osrc]) {
+                if (e.nbr == src) {
+                    isCyclic = true;
+                    break;
+                }
+            }
+
+            if (isCyclic == true) {
+                System.out.println("*");
+            } else {
+                System.out.println(".");
+            }
+            return;
+        }
+        vis.add(src);
+        for (Edge e : graph[src]) {
+            int nbr = e.nbr;
+            if (vis.contains(nbr) == false) {
+                hamiltonian(graph, nbr, osrc, vis, psf + src);
+            }
+        }
+        vis.remove(src);
+    }
+
+    public static class BFSPair {
+        int vtx;
+        String psf;
+
+        BFSPair(int vtx, String psf) {
+            this.vtx = vtx;
+            this.psf = psf;
+        }
+    }
+
+    public static void bfs(ArrayList<Edge>[] graph, int src) {
+        Queue<BFSPair> qu = new LinkedList<>();
+        qu.add(new BFSPair(src, ""));
+        boolean vis[] = new boolean[graph.length];
+
+        while (qu.size() > 0) {
+            // get+remove
+            BFSPair rem = qu.remove();
+
+            // mark
+            if (vis[rem.vtx] == true) {
+                continue;
+            } else {
+                vis[rem.vtx] = true;
+            }
+
+            // work->printing
+            System.out.println(rem.vtx + "@" + rem.psf);
+
+            // add unvisited neighbours;
+            for (Edge e : graph[rem.vtx]) {
+                if (vis[e.nbr] == false) {
+                    qu.add(new BFSPair(e.nbr, rem.psf + e.nbr));
+                }
+            }
+
+        }
+    }
+
+    public static boolean isCyclic(ArrayList<Edge>[] graph) {
+        boolean vis[] = new boolean[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (vis[i] != true) {
+                boolean res = bfsForCycle(graph, i, vis);
+                if (res == true)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean bfsForCycle(ArrayList<Edge>[] graph, int src, boolean vis[]) {
+        Queue<Integer> qu = new LinkedList<>();
+        qu.add(src);
+
+        while (qu.size() > 0) {
+            // get+remove
+            int rem = qu.remove();
+
+            // mark
+            if (vis[rem] == true) {
+                // already visited so cycle is detected
+                return true;
+            } else {
+                // mark
+                vis[rem] = true;
+            }
+
+            // // work->printing
+            // System.out.println(rem.vtx + "@" + rem.psf);
+
+            // add unvisited neighbours;
+            for (Edge e : graph[rem]) {
+                if (vis[e.nbr] == false) {
+                    qu.add(e.nbr);
+                }
+            }
+
+        }
+        return false;
+    }
+
+    public static boolean isCyclic2(ArrayList<Edge>[] graph) {
+
+        boolean vis[] = new boolean[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (vis[i] != true) {
+                boolean res = dfs(graph, i, -1, vis);
+                if (res == true)
+                    return true;
+            }
+        }
+        return false;
+
+    }
+
+    public static boolean dfs(ArrayList<Edge>[] graph, int src, int parent, boolean vis[]) {
+        vis[src] = true;
+        for (Edge e : graph[src]) {
+            int nbr = e.nbr;
+            if (vis[nbr] == true && nbr != parent) {
+                // cycle is present
+                return true;
+            }
+            if (vis[nbr] != true) {
+                boolean res = dfs(graph, nbr, src, vis);
+                if (res == true)
+                    return true;
+            }
+        }
+        return false;
+
+    }
+
+    public static class BipPair {
+        int vtx;
+        int dtime;
+
+        BipPair(int vtx, int dtime) {
+            this.vtx = vtx;
+            this.dtime = dtime;
+        }
+    }
+
+    public static boolean bipartite(ArrayList<Edge>[] graph) {
+        int vis[] = new int[graph.length];
+        Arrays.fill(vis, -1);
+        for (int i = 0; i < graph.length; i++) {
+            if (vis[i] == -1) {
+                boolean res = bfscyclic(graph, vis, i);
+                if (res == false)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean bfscyclic(ArrayList<Edge>[] graph, int vis[], int src) {
+        Queue<BipPair> qu = new LinkedList<>();
+        qu.add(new BipPair(src, 0));
+        while (qu.size() > 0) {
+            BipPair rem = qu.remove();
+
+            if (vis[rem.vtx] == -1) {
+                // mark
+                vis[rem.vtx] = rem.dtime;
+            } else {
+                // already discovered
+                // 1.if discovered with sam level;
+                if (vis[rem.vtx] == rem.dtime)
+                    continue;
+                else
+                    return false;
+
+                // 2.if again discovered with different dtime->return false
+                // becuase cycle is odd
+
+            }
+
+            for (Edge e : graph[rem.vtx]) {
+                int nbr = e.nbr;
+                if (vis[nbr] == -1) {
+                    qu.add(new BipPair(nbr, rem.dtime + 1));
+                }
+            }
+
+        }
+        return true;
+
+    }
+
+    public static int spreadInfection(ArrayList<Edge>[] graph, int src, int t) {
+        int count = 0;
+        Queue<BipPair> qu = new LinkedList<>();
+        int[] vis = new int[graph.length];
+        qu.add(new BipPair(src, 1));
+        while (qu.size() > 0) {
+            BipPair rem = qu.remove();
+            if (vis[rem.vtx] != 0) {
+                // marked
+                continue;
+            } else {
+                vis[rem.vtx] = rem.dtime;
+            }
+
+            if (rem.dtime > t)
+                break;
+            count++;
+
+            for (Edge e : graph[rem.vtx]) {
+                if (e.nbr != 0) {
+                    qu.add(new BipPair(e.nbr, rem.dtime + 1));
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static class DPair implements Comparable<DPair> {
+        int vtx;
+        String psf;
+
+        int wsf;
+
+        DPair(int vtx, String psf, int wsf) {
+            this.vtx = vtx;
+            this.wsf = wsf;
+            this.psf = psf;
+        }
+
+        public int compareTo(DPair o) {
+            return this.wsf - o.wsf;
+        }
+    }
+
+    public static void dijikstras(ArrayList<Edge>[] graph, int src) {
+        PriorityQueue<DPair> qu = new PriorityQueue<>();
+        qu.add(new DPair(src, "" + src, 0));
+        boolean vis[] = new boolean[graph.length];
+        while (qu.size() > 0) {
+            // 1.get+remove
+            DPair rem = qu.remove();
+
+            if (vis[rem.vtx] == true)
+                continue;
+
+            // 2.Mark *
+            if (vis[rem.vtx] == true)
+                continue;
+
+            vis[rem.vtx] = true;
+
+            // 3.Work-Print paths
+            System.out.println(rem.vtx + " via " + rem.psf + " @ " + rem.wsf);
+
+            // 4.Add neighbours
+            for (Edge e : graph[rem.vtx]) {
+                int nbr = e.nbr;
+                if (vis[nbr] != true) {
+                    qu.add(new DPair(nbr, rem.psf + nbr, rem.wsf + e.wt));
+                }
+            }
+        }
+    }
+
+    public static class Phelper implements Comparable<Phelper> {
+        int vtx;
+        int parent;
+        int wt;
+
+        Phelper(int vtx, int parent, int wt) {
+            this.vtx = vtx;
+            this.parent = parent;
+            this.wt = wt;
+        }
+
+        public int compareTo(Phelper o) {
+            return this.wt - o.wt;
+        }
+    }
+
+    // minimum spanning tree to connect all the edges
+    // should be acyclic and connected
+    public static void Prims(ArrayList<Edge>[] graph) {
+        PriorityQueue<Phelper> pq = new PriorityQueue<>();
+        ArrayList<Edge>[] mst = new ArrayList[graph.length];
+        boolean vis[] = new boolean[graph.length];
+        for (int i = 0; i < graph.length; i++)
+            mst[i] = new ArrayList<>();
+
+        pq.add(new Phelper(0, -1, 0));
+        while (pq.size() > 0) {
+            // 1.get+remove
+            Phelper rem = pq.remove();
+
+            // 2.mark
+            if (vis[rem.vtx] == true)
+                continue;
+
+            vis[rem.vtx] = true;
+
+            // 3work->print for question + add edge for mst graph
+            if (rem.parent != -1) {
+                System.out.println("[" + rem.vtx + "-" + rem.parent + "@" + rem.wt + "]");
+                addEdge(mst, rem.vtx, rem.parent, rem.wt);
+            }
+
+            // 4add siblings
+            for (Edge e : graph[rem.vtx]) {
+                if (vis[e.nbr] != true) {
+                    pq.add(new Phelper(e.nbr, rem.vtx, e.wt));
+
+                }
+            }
+        }
+        display(mst);
+
+    }
+
     public static void fun() {
 
         ArrayList<Edge> graph[] = createGraph();
         // display(graph);
-        boolean vis[] = new boolean[7];
-        // System.out.println(hasPath(graph, 0, 6, vis));
-        // printAllPaths(graph, 0, 6, vis, "");
-        // printAllPathswithsum(graph, 0, 6, vis, "", 0);
-        multisolver(graph, 0, 6, vis, "", 0, 30, 4);
-        System.out.println("Smallest Path:=" + minpath + "@" + miwt);
-        System.out.println("Largest Path:=" + maxpath + "@" + mwt);
-        System.out.println("Just Larger Path than 30=" + justmaxpath + "@" + ceil);
-        System.out.println("Just Smaller Path than 30=" + justminpath + "@" + floor);
-        System.out.println(4 + "th largest path = " + pq.peek().psf + "@" + pq.peek().wt);
+        // boolean vis[] = new boolean[7];
+        // // System.out.println(hasPath(graph, 0, 6, vis));
+        // // printAllPaths(graph, 0, 6, vis, "");
+        // // printAllPathswithsum(graph, 0, 6, vis, "", 0);
+        // multisolver(graph, 0, 6, vis, "", 0, 30, 4);
+        // System.out.println("Smallest Path:=" + minpath + "@" + miwt);
+        // System.out.println("Largest Path:=" + maxpath + "@" + mwt);
+        // System.out.println("Just Larger Path than 30=" + justmaxpath + "@" + ceil);
+        // System.out.println("Just Smaller Path than 30=" + justminpath + "@" + floor);
+        // System.out.println(4 + "th largest path = " + pq.peek().psf + "@" +
+        // pq.peek().wt);
+        // HashSet<Integer> vis = new HashSet<>();
+        // hamiltonian(graph, 0, 0, vis, "");
+        // dijikstras(graph, 0);
+        Prims(graph);
     }
 
     public static void main(String[] args) {
---
+++ dsa-level1/graphs/perfectFriend.java
@@ -0,0 +1,78 @@
+import java.net.Inet4Address;
+import java.util.*;
+
+public class perfectFriend {
+
+    public static void gcc(ArrayList<Integer>[] graph, int src, boolean vis[], ArrayList<Integer> comp) {
+        vis[src] = true;
+        comp.add(src);
+        for (int nbr : graph[src]) {
+            if (vis[nbr] == false) {
+                gcc(graph, nbr, vis, comp);
+            }
+
+        }
+    }
+
+    public static int perfectFr(ArrayList<Integer>[] graph) {
+        int n = graph.length;
+        boolean vis[] = new boolean[n];
+        ArrayList<ArrayList<Integer>> comps = new ArrayList<>();
+        for (int v = 0; v < n; v++) {
+            if (vis[v] != true) {
+                ArrayList<Integer> comp = new ArrayList<>();
+                gcc(graph, v, vis, comp);
+                comps.add(comp);
+            }
+        }
+
+        // find count with different appraoches
+        // method 1
+        // int count = 0;
+        // for (int i = 0; i < comps.size(); i++) {
+        //     int sz1 = comps.get(i).size();
+        //     for (int j = i + 1; j < comps.size(); j++) {
+        //         int sz2 = comps.get(j).size();
+        //         count += (sz1 * sz2);
+
+        //     }
+        // }
+
+        // return count;
+
+        //method 2;
+        int sum=comps.get(comps.size()-1).size();
+        for(int i=comps.size()-2;i>=0;i--)
+        {
+            int s=comps.get(i).size();
+            count+=sum*s;
+            sum+=s;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt(); // number of vertices
+        int k = scn.nextInt(); // number of edges
+
+        ArrayList<Integer>[] graph = new ArrayList[n];
+
+        for (int i = 0; i < n; i++)
+            graph[i] = new ArrayList<>();
+
+        // add edges in graph k times;
+        for (int i = 0; i < k; i++) {
+            int src = scn.nextInt();
+            int nbr = scn.nextInt();
+
+            graph[src].add(nbr);
+            graph[nbr].add(src);
+        }
+
+        // no of pairs of perfect friens,count so that both are not in same club
+        int count = perfectFr(graph);
+        System.out.println(count);
+    }
+}
