ab5acba0a4f39635fcad73adfe13374300a8c1d1
Thammineni <mithun.ram.kumar.thammineni@sap.com>
2021-08-08 13:06:46 +0530

    Updated file

--- dsa-level2/linkedlist.java
+++ dsa-level2/linkedlist.java
@@ -5,6 +5,7 @@ public class linkedlist {
     public static class ListNode {
         int val;
         ListNode next;
+        ListNode random;
 
         ListNode() {
         }
@@ -388,6 +389,276 @@ public class linkedlist {
 
         ListNode res = mergeTwoLists(l1, l2);
         return res;
+    }
+
+    // clone of linked list
+    public static ListNode copyLinkedList(ListNode head) {
+        ListNode h1 = new ListNode(-1);
+        ListNode temp1 = h1;
+        ListNode temp2 = head;
+
+        while (temp2 != null) {
+            ListNode nn = new ListNode(temp2.val);
+            temp1.next = nn;
+            temp1 = temp1.next;
+
+            temp2 = temp2.next;
+        }
+
+        return h1.next;
+    }
+
+    public static ListNode copyRandomList(ListNode head1) {
+        // 1.makke a clone of linked list with next pointer only
+        ListNode head2 = copyLinkedList(head1);
+
+        // 2.make a zigzag pattern
+        ListNode t1 = head1;
+        ListNode t2 = head2;
+        while (t1 != null && t2 != null) {
+            ListNode n1 = t1.next;
+            ListNode n2 = t2.next;
+            t1.next = t2;
+            t2.next = n1;
+            t1 = n1;
+            t2 = n2;
+        }
+
+        // 3.make connnection of random pointer
+        t1 = head1;
+        while (t1 != null) {
+            t1.next.random = t1.random == null ? null : t1.random.next;
+            t1 = t1.next.next;
+        }
+
+        // 4.Regain original list
+        t1 = head1;
+        t2 = head2;
+        while (t1 != null && t2 != null) {
+            ListNode n1 = t2.next;
+            ListNode n2 = n1 == null ? null : n1.next;
+
+            t1.next = n1;
+            t2.next = n2;
+
+            t1 = n1;
+            t2 = n2;
+
+        }
+
+        return head2;
+    }
+
+    // clone of linked list->using extra space ie.hash Map
+    public static ListNode copyLinkedList2(ListNode head1) {
+
+        // copy of linked list with next
+        ListNode head2 = copyLinkedList(head1);
+
+        // 2.make hashmap of node vs node1
+        HashMap<ListNode, ListNode> map = new HashMap<>();
+        ListNode t1 = head1;
+        ListNode t2 = head2;
+
+        while (t1 != null) {
+            map.put(t1, t2);
+            t1 = t1.next;
+            t2 = t2.next;
+        }
+
+        // 3.Make connection of random pointer
+        for (ListNode onode : map.keySet()) {
+            // onode->original node;
+            // cnode->cloned node
+            ListNode cnode = map.get(onode);
+            ListNode orandom = onode.random; // orignal random
+
+            cnode.random = orandom == null ? null : map.get(orandom);
+
+        }
+
+        return head2;
+    }
+
+    public static ListNode segregateevenodd(ListNode head) {
+        ListNode temp = head;
+        ListNode odummy = new ListNode(-1);
+        ListNode edummy = new ListNode(-1);
+        ListNode t1 = odummy, t2 = edummy;
+        while (temp != null) {
+            if (temp.val % 2 == 0) {
+                t2.next = temp;
+                t2 = temp;
+            } else {
+                t1.next = temp;
+                t1 = temp;
+            }
+            temp = temp.next;
+        }
+        t1.next = null;
+        t2.next = null;
+        // add lists accordingly even then odd;
+        t2.next = odummy.next;
+        return edummy.next;
+
+    }
+
+    // swap only data
+    public static ListNode segregate01(ListNode head) {
+        ListNode i = head;
+        ListNode j = head;
+        while (i != null) {
+            if (i.val == 1) {
+                i = i.next;
+            } else {
+                int temp = i.val;
+                i.val = j.val;
+                j.val = temp;
+                i = i.next;
+                j = j.next;
+
+            }
+        }
+        return head;
+    }
+
+    // seggregate 0,1,2 by swapping nodes
+    public static ListNode segregate012(ListNode head) {
+        ListNode dummy0 = new ListNode(-1);
+        ListNode dummy1 = new ListNode(-1);
+        ListNode dummy2 = new ListNode(-1);
+
+        ListNode zero = dummy0;
+        ListNode one = dummy1;
+        ListNode two = dummy2;
+
+        ListNode temp = head;
+        while (temp != null) {
+            if (temp.val == 0) {
+                zero.next = temp;
+                zero = temp;
+            } else if (temp.val == 1) {
+                one.next = temp;
+                one = temp;
+            } else {
+                two.next = temp;
+                two = temp;
+            }
+            temp = temp.next;
+        }
+
+        // connect pointer according to dependencies from hight to low
+
+        one.next = dummy2.next;
+        zero.next = dummy1.next;
+        two.next = null;
+
+        return dummy0.next;
+    }
+
+    // seggregate 0,1,2 by swapping data
+    public static void swapNodeData(ListNode n1, ListNode n2) {
+        int temp = n1.val;
+        n1.val = n2.val;
+        n2.val = temp;
+    }
+
+    public static ListNode segregate012swapdata(ListNode head) {
+
+        ListNode i = head, j = head, k = head;
+        // i->first unsolved;
+        // j>first 2;
+        // k->first 1;
+        while (i != null) {
+            if (i.val == 0) {
+                // swap i,j
+                swapNodeData(i, j);
+                // swap j,k
+                swapNodeData(j, k);
+
+                i = i.next;
+                j = j.next;
+                k = k.next;
+
+            } else if (i.val == 1) {
+                // swap i,j
+                swapNodeData(i, j);
+                i = i.next;
+                j = j.next;
+            } else {
+                i = i.next;
+            }
+        }
+        return head;
+
+    }
+
+    // seggregate based on last index;
+    public static ListNode segregateOnLastIndex(ListNode head) {
+
+        // get pivot ie.last indx
+        ListNode pivot = head;
+        while (pivot.next != null) {
+            pivot = pivot.next;
+        }
+
+        // seggreate
+        ListNode smaller = new ListNode(-1);
+        ListNode greater = new ListNode(-1);
+
+        ListNode t1 = smaller;
+        ListNode t2 = greater, temp = head;
+        while (temp != null) {
+            // smaller <= pivot
+            if (temp.val <= pivot.val) {
+                t1.next = temp;
+                t1 = temp;
+            } else {
+                // greater than pivot
+                t2.next = temp;
+                t2 = temp;
+
+            }
+            temp = temp.next;
+        }
+
+        t2.next = null;
+        t1.next = greater.next;
+
+        return pivot;
+
+    }
+
+    public static ListNode segregate(ListNode head, int pivotIdx) {
+        ListNode pivot = head;
+        // 1.find pivotnode
+        for (int i = 0; i < pivotIdx; i++)
+            pivot = pivot.next;
+        // 2.seggregat on basis of data and skp pivot node to add it later in middle;
+        ListNode smaller = new ListNode(-1);
+        ListNode greater = new ListNode(-1);
+        ListNode t1 = smaller, t2 = greater, temp = head;
+
+        while (temp != null) {
+            if (pivot != temp) {
+                if (temp.val <= pivot.val) {
+                    t1.next = temp;
+                    t1 = temp;
+                } else {
+                    t2.next = temp;
+                    t2 = temp;
+
+                }
+            }
+            temp = temp.next;
+        }
+
+        t2.next = null;
+        pivot.next = greater.next;
+        t1.next = pivot;
+
+        return smaller.next;
+
     }
 
     public static void ques() {
