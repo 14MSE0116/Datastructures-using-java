09793c4a7e1feebfa8e055c5cb512f6f30f3304f
Thammineni <mithun.ram.kumar.thammineni@sap.com>
2021-10-09 22:16:37 +0530

    Updated  file

--- dsa-level2/graphs.java
+++ dsa-level2/graphs.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+import array.pair;
+
 public class graphs {
     static class Edge {
         int nbr;
@@ -301,6 +303,257 @@ public class graphs {
         Pair01(int x, int y) {
             this.x = x;
             this.y = y;
+        }
+    }
+
+    // leetcode 1162. https://leetcode.com/problems/as-far-from-land-as-possible/
+    private static class pairD {
+        int x;
+        int y;
+
+        pairD(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public int maxDistance(int[][] grid) {
+        // 1.Iterate on grid and add all one's in queue and mark it as well
+        Queue<pairD> qu = new LinkedList<>();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    qu.add(new pairD(i, j));
+                    grid[i][j] = -1;
+                }
+            }
+        }
+        if (qu.size() == 0 || qu.size() == grid.length * grid[0].length)
+            return -1;
+        // Run BFS
+        int level = -1;
+        while (qu.size() > 0) {
+            level++;
+            int size = qu.size();
+            while (size-- > 0) {
+                pairD rem = qu.remove();
+                for (int d = 0; d < 4; d++) {
+                    int r = rem.x + xdir[d];
+                    int c = rem.y + ydir[d];
+                    if (r >= 0 && r < grid.length && c >= 0 && c < grid[0].length && grid[r][c] != -1) {
+                        qu.add(new pairD(r, c));
+                        grid[r][c] = -1;
+                    }
+                }
+            }
+        }
+
+        return level;
+
+    }
+
+    // Pepcoding->Find Mother vertex->top of toplogical sort
+    public static int findMotherVertex(int N, ArrayList<ArrayList<Integer>> graph) {
+        boolean vis[] = new boolean[N];
+        Stack<Integer> st = new Stack<>();
+        for (int v = 0; v < N; v++) {
+            if (vis[v] == false) {
+                dfsForStack(graph, v, vis, st);
+            }
+        }
+        int top = st.peek();
+        count = 0;
+        vis = new boolean[N];
+        dfsForcount(graph, st.peek(), vis);
+        return count == N ? st.peek() + 1 : -1;
+    }
+
+    private static int count = 0;
+
+    private static void dfsForcount(ArrayList<ArrayList<Integer>> graph, int src, boolean vis[]) {
+        vis[src] = true;
+        count++;
+        for (int nbr : graph.get(src)) {
+            if (vis[nbr] == false) {
+                dfsForcount(graph, nbr, vis);
+            }
+        }
+
+    }
+
+    private static void dfsForStack(ArrayList<ArrayList<Integer>> graph, int src, boolean vis[], Stack<Integer> st) {
+        vis[src] = true;
+        for (int nbr : graph.get(src)) {
+            if (vis[nbr] == false) {
+                dfsForStack(graph, nbr, vis, st);
+            }
+        }
+        st.push(src);
+
+    }
+
+    // leetcode 934:https://leetcode.com/problems/shortest-bridge/
+    public int shortestBridge(int[][] grid) {
+        // 1.add all one in queue,using DFS from first on encountered
+        Queue<pairD> qu = new LinkedList<>();
+        for (int i = 0; i < grid.length; i++) {
+            boolean flag = true;
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    dfsShortestBridge(grid, i, j, qu);
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag == false) {
+
+                break;
+            }
+        }
+
+        // 2.applly bfs and check first one eoncounteres
+        int lvl = -1;
+        while (qu.size() > 0) {
+            lvl++;
+            int sz = qu.size();
+            while (sz-- > 0) {
+                pairD rem = qu.remove();
+                for (int d = 0; d < xdir.length; d++) {
+                    int r = rem.x + xdir[d];
+                    int c = rem.y + ydir[d];
+                    if (r >= 0 && r < grid.length && c >= 0 && c < grid[0].length && grid[r][c] != -1) {
+                        if (grid[r][c] == 1)
+                            return lvl;
+                        else {
+                            grid[r][c] = -1;
+                            qu.add(new pairD(r, c));
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    static void dfsShortestBridge(int[][] grid, int x, int y, Queue<pairD> qu) {
+        grid[x][y] = -1;
+        qu.add(new pairD(x, y));
+        for (int d = 0; d < xdir.length; d++) {
+            int r = x + xdir[d];
+            int c = y + ydir[d];
+            if (r >= 0 && r < grid.length && c >= 0 && c < grid[0].length && grid[r][c] == 1) {
+                dfsShortestBridge(grid, r, c, qu);
+            }
+        }
+    }
+
+    // Course Scheduling 1
+    // https://leetcode.com/problems/course-schedule/
+    // Using Khans algorithm
+    public boolean canFinish(int n, int[][] edges) {
+        // prepare graph
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+        }
+
+        // Make an indegree array from graph
+        int[] indegree = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int nbr : graph.get(i)) {
+                indegree[nbr]++;
+            }
+        }
+
+        // add element in queue which have 0 indegree
+        Queue<Integer> qu = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0)
+                qu.add(i);
+        }
+
+        int count = 0;
+        while (qu.size() > 0) {
+            // 1.get+remove
+            int rem = qu.remove();
+
+            // 2.work
+            count++;
+
+            // Decrease indegree of nbr and if indegree becomes 0 ,add in queue
+            for (int nbr : graph.get(rem)) {
+                indegree[nbr]--;
+                if (indegree[nbr] == 0)
+                    qu.add(nbr);
+            }
+
+           
+
+        }
+        return count == n;
+
+    }
+
+    //course schedule 2
+    public int[] findOrder(int n, int[][] edges) {
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+        }
+
+        // Make an indegree array from graph
+        int[] indegree = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int nbr : graph.get(i)) {
+                indegree[nbr]++;
+            }
+        }
+
+        // add element in queue which have 0 indegree
+        Queue<Integer> qu = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0)
+                qu.add(i);
+        }
+
+        int count = 0;
+        Stack<Integer>st=new Stack<>();
+        while (qu.size() > 0) {
+            // 1.get+remove
+            int rem = qu.remove();
+
+            // 2.work
+            st.push(rem);
+            count++;
+
+            // Decrease indegree of nbr and if indegree becomes 0 ,add in queue
+            for (int nbr : graph.get(rem)) {
+                indegree[nbr]--;
+                if (indegree[nbr] == 0)
+                    qu.add(nbr);
+            }
+
+           
+
+        }
+        if(count==n){
+            int []res=new int[0];
+            return res;
+        }
+        else{
+            int idx=0;
+            int res[]=new int[count];
+            while(st.size()>0)
+              res[idx++]=st.pop();
+            return res;  
         }
     }
 
