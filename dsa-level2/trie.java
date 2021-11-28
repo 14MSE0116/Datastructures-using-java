414262aafcfcc4fe8218888d3067d4f700cb7480
Mithun Thammineni <narmithun@gmail.com>
2021-11-28 14:15:09 +0530

    Updated file

--- dsa-level2/trie.java
+++ dsa-level2/trie.java
@@ -130,6 +130,187 @@ class trie {
         }
     }
 
+    //https://leetcode.com/problems/word-search-ii/
+    private class Node {
+        Node[] children;
+        boolean isEnd;
+        int freq;
+
+        Node() {
+            this.children = new Node[26];
+            this.isEnd = false;
+            this.freq = 0;
+        }
+    }
+
+    private void insert(String word, Node root) {
+        //make a trie and add words in it
+        Node root1 = new Node();
+
+    }
+
+    public List<String> findWords(char[][] board, String[] words) {
+
+    }
+
+    //leetcode677 https://leetcode.com/problems/map-sum-pairs/
+    class MapSum {
+        private class Node {
+            Node children[];
+            int impact;
+
+            Node() {
+                this.children = new Node[26];
+                this.impact = 0;
+            }
+        }
+
+        private HashMap<String, Integer> map;
+        private Node root;
+
+        public MapSum() {
+            root = new Node();
+            this.map = new HashMap<>();
+        }
+
+        public void insert(String key, int val) {
+            int oldval = map.getOrDefault(key, 0);
+            int newval = val;
+            int impact = newval - oldval;
+            map.put(key, val);
+
+            Node ptr = root;
+            for (int i = 0; i < key.length(); i++) {
+                char ch = key.charAt(i);
+                if (ptr.children[ch - 'a'] == null) {
+                    ptr.children[ch - 'a'] = new Node();
+                }
+                ptr = ptr.children[ch - 'a'];
+                ptr.impact += impact;
+            }
+
+
+        }
+
+        public int sum(String prefix) {
+            Node ptr = root;
+            for (int i = 0; i < prefix.length(); i++) {
+                char ch = prefix.charAt(i);
+                if (ptr.children[ch - 'a'] == null) {
+                    return 0;
+                }
+                ptr = ptr.children[ch - 'a'];
+            }
+            return ptr.impact;
+
+        }
+    }
+
+    //https://leetcode.com/problems/longest-word-in-dictionary/
+    private class Nodee {
+        Nodee children[];
+        String s;
+
+        Nodee() {
+            this.children = new Nodee[26];
+            this.s = null;
+        }
+    }
+
+    private void insert(Nodee root, String word) {
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if (root.children[ch - 'a'] == null) {
+                root.children[ch - 'a'] = new Nodee();
+            }
+            root = root.children[ch - 'a'];
+        }
+        root.s = word;
+    }
+
+    static String str;
+
+    public String longestWord(String[] words) {
+        Nodee root = new Nodee();
+        for (String word : words) {
+            insert(root, word);
+        }
+        str = "";
+        for (int i = 0; i < 26; i++) {
+            if (root.children[i] != null)
+                dfsTrie(root.children[i]);
+        }
+        return str;
+
+    }
+
+    void dfsTrie(Nodee node) {
+        if (node.s == null)
+            return;
+        if (node.s.length() > str.length())
+            str = node.s;
+        for (int i = 0; i < 26; i++) {
+            if (node.children[i] != null)
+                dfsTrie(node.children[i]);
+        }
+
+    }
+
+    class StreamChecker {
+
+        private class Node {
+            Node[] children;
+            boolean isEnd;
+
+            Node() {
+                this.children = new Node[26];
+                this.isEnd = false;
+            }
+        }
+
+        private Node root;
+        private StringBuilder stream;
+
+        private void insert(Node node, String word) {
+            for (int i = word.length() - 1; i >= 0; i--) {
+                char ch = word.charAt(i);
+                if (node.children[ch - 'a'] == null) {
+                    node.children[ch - 'a'] = new Node();
+                }
+                node = node.children[ch - 'a'];
+            }
+            node.isEnd = true;
+        }
+
+        public StreamChecker(String[] words) {
+            root = new Node();
+            stream = new StringBuilder();
+            for (String word : words)
+                insert(root, word);
+
+        }
+
+        public boolean query(char letter) {
+            stream.append(letter);
+            return find();
+        }
+
+        private boolean find() {
+            Node temp = root;
+            for (int i = stream.length() - 1; i >= 0; i--) {
+                char ch = stream.charAt(i);
+                if (temp.children[ch - 'a'] == null)
+                    return false;
+                temp = temp.children[ch - 'a'];
+
+                if (temp.isEnd == true)
+                    return true;
+
+            }
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
 
     }
